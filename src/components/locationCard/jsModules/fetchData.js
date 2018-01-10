884a2f66325d4cd5c380552f33c702b054f65e3f
Nick Hendricks <moabs81@hotmail.com>
2018-01-10 00:13:47 -0500

    updated fetchData to use external JSON mock and re-built project

--- src/components/locationCard/jsModules/fetchData.js
+++ src/components/locationCard/jsModules/fetchData.js
@@ -2,8 +2,9 @@ import xHrReq from '../../workBenchContainer/jsModules/xHrReq';
 
 const fetchData = (cbReturn) => {
     const xHrReqParams = {
-        baseURI: 'http://localhost:3000',
-        searchURI: '/officeCards',
+        //baseURI: 'http://localhost:3000',
+        baseURI: 'https://my-json-server.typicode.com/moabs81/fakeJSONServer',
+        searchURI: '/officeCards100',
         method: 'GET',
         success: function (result) {
             cbReturn(result);
